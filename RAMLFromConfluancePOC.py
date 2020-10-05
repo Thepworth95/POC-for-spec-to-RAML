e5c59f461c968bc00d9f685f42cfb8efce5f21ae
james-work-account <31282758+james-work-account@users.noreply.github.com>
2020-10-05 16:24:08 +0100

    non-formatted brackets need double brackets

--- RAMLFromConfluancePOC.py
+++ RAMLFromConfluancePOC.py
@@ -1,87 +1,82 @@
 import requests
-import json 
-from lxml import html
 from bs4 import BeautifulSoup
+import creds
 
-def retrieveData(RequstUrl):
 
+def retrieve_data(request_url):
     # This URL will be the URL that your login form points to with the "action" tag.
-    PostLoginUrl = 'https://confluence.tools.tax.service.gov.uk/login.action'
+    POST_LOGIN_URL = 'https://confluence.tools.tax.service.gov.uk/login.action'
 
     payload = {
-        'os_username': '',
-        'os_password': ''
+        'os_username': creds.username,
+        'os_password': creds.password
     }
 
-    # This fetches the data and seperates the parts we need for use in the RAML
+    # This fetches the data and separates the parts we need for use in the RAML
     with requests.Session() as session:
-        post = session.post(PostLoginUrl, data=payload)
-        page = session.get(RequstUrl)
+        session.post(POST_LOGIN_URL, data=payload)
+        page = session.get(request_url)
         soup = BeautifulSoup(page.text, 'html.parser')
-        listOfvalues = soup.find_all("td", class_ = "confluenceTd")
+        list_of_values = soup.find_all("td", class_="confluenceTd")
         result = []
 
-        for item in listOfvalues:
-            if item.p != None:
+        for item in list_of_values:
+            if item.p is not None:
                 result.append(item.p.text)
-            elif item.div != None:
+            elif item.div is not None:
                 result.append(item.div.text)
-            elif item.strong != None:
+            elif item.strong is not None:
                 result.append(item.strong.text)
             else:
                 result.append(item.text)
-        return(result)
+        return result
 
 
-
-
-
-# This function seperates out the relevent data for the path Parameters and presents them in the same style used in the RAML
-def retrievePathParameters(RequstUrl):
-    data = retrieveData(RequstUrl)
-    res = data.index(u'Path Parameter')
-    anotherPathParameter = True
-    responseData = []
-    while anotherPathParameter:
-        responseData.append([data[res+1], data[res+2], data[res+3]])
-        if data[res+11] == u'Path Parameter': 
+# This function separates out the relevant data for the path Parameters and presents them in the same style used in
+# the RAML
+def retrieve_path_parameters(request_url):
+    data = retrieve_data(request_url)
+    res = data.index('Path Parameter')
+    another_path_parameter = True
+    response_data = []
+    while another_path_parameter:
+        response_data.append([data[res + 1], data[res + 2], data[res + 3]])
+        if data[res + 11] == 'Path Parameter':
             res = res + 11
         else:
-            anotherPathParameter = False
+            another_path_parameter = False
     response = []
-    for item in responseData:
-        pathParameter = """uriParameters:
+    for item in response_data:
+        path_parameter = """uriParameters:
                 {}:
                     description: {}
                     type: {}
                     example: Insert Example Here
             """.format(
-        item[0],
-        item[1],
-        item[2]
+            item[0],
+            item[1],
+            item[2]
         )
-        response.append(pathParameter)
+        response.append(path_parameter)
     print(response)
-    return(response)
+    return response
 
 
-
-
-
-# This function seperates out the relevent data for the query Parameters and presents them in the same style used in the RAML
-def retrieveQueryParameters(RequstUrl):
-    data = retrieveData(RequstUrl)
-    res = data.index(u'Query Parameter')
-    anotherPathParameter = True
-    responseData = []
-    while anotherPathParameter:
-        responseData.append([data[res+1], data[res+2], data[res+5]])
-        if data[res+11] == u'Query Parameter': 
+# This function separates out the relevant data for the query Parameters and presents them in the same style used in
+# the RAML 
+def retrieve_query_parameters(request_url):
+    data = retrieve_data(request_url)
+    res = data.index('Query Parameter')
+    another_path_parameter = True
+    response_data = []
+    while another_path_parameter:
+        response_data.append([data[res + 1], data[res + 2], data[res + 5]])
+        if data[res + 11] == 'Query Parameter':
             res = res + 11
         else:
-            anotherPathParameter = False
+            another_path_parameter = False
     response = []
-    for item in responseData:
+    for item in response_data:
         response.append("""
             {}:
                 queryParameters:
@@ -90,29 +85,28 @@ def retrieveQueryParameters(RequstUrl):
                         example: "---Insert Example Here---"
                         required: {}
             """.format(
-        item[0],
-        item[0],
-        item[1],
-        item[2]==u'M'
+            item[0],
+            item[0],
+            item[1],
+            item[2] == 'M'
         ))
     print(response)
-    return(response)
+    return response
 
 
-
-
-# This function seperates out the relevent data for the Request body schema and presents it in the same style used in the RAML schema's
-def retrieveRequestBodySchema(RequstUrl, firstField):
-    data = retrieveData(RequstUrl)
-    anotherPathParameter = True
-    responseData = []
-    res = data.index(firstField)
-    while anotherPathParameter:
-        responseData.append([data[res], data[res+1], data[res+2]])
-        if data[res+9] != u'O1': 
+# This function separates out the relevant data for the Request body schema and presents it in the same style used in
+# the RAML schema's 
+def retrieve_request_body_schema(request_url, first_field):
+    data = retrieve_data(request_url)
+    another_path_parameter = True
+    response_data = []
+    res = data.index(first_field)
+    while another_path_parameter:
+        response_data.append([data[res], data[res + 1], data[res + 2]])
+        if data[res + 9] != 'O1':
             res = res + 10
         else:
-            anotherPathParameter = False
+            another_path_parameter = False
     response = """{
         "$schema": "http://json-schema.org/draft-04/schema#",
         "title": "---Insert title---",
@@ -120,109 +114,117 @@ def retrieveRequestBodySchema(RequstUrl, firstField):
         "type": "object",
         "properties": {
     """
-    for item in responseData:
+    for item in response_data:
         print(response)
         print(item)
-        if item[2]==u'Array':
+        element = ""
+        if item[2] == 'Array':
             response = response + """
-            {}: {
+            {}: {{
                 "type": "Array",
                 "description": {},
-                "items": {
+                "items": {{
                     "type": "object",
-                    "properties": {
+                    "properties": {{
             """.format(
                 item[0],
                 item[1]
             )
-        elif item[2]==u'Object':
+        elif item[2] == 'Object':
             pass
-        elif item[2]==u'string (date)':
+        elif item[2] == 'string (date)':
             element = """
-            {}: {
-                "description": {},
-                "type": {},
+            "{}": {{
+                "description": "{}",
+                "type": "string",
                 "example": "---Insert example from description---"
-            }""".format(
+            }}""".format(
                 item[0],
-                item[1],
-                u'string'
+                item[1]
             )
-        elif item[2]==u'string':
+        elif item[2] == 'string':
             element = """
-            {}: {
+            {}: {{
                 "description": {},
                 "type": {},
                 "example": "---Insert example from description---"
-            }""".format(
+            }}""".format(
                 item[0],
                 item[1],
                 item[2]
             )
         response = response + element
-    response = response + """  },
+    response = response + """  }},
         "additionalProperties": false
-    }
+    }}
     """
     print(response)
-    return(response)
+    return response
 
 
-
-
-
-# This function seperates out the relevent data for the Response body schema and presents it in the same style used in the RAML schema's
-def retrieveResponseBodySchema(RequstUrl):
-    data = retrieveData(RequstUrl)
-    anotherPathParameter = True
+# This function separates out the relevant data for the Response body schema and presents it in the same style used 
+# in the RAML schema's 
+def retrieve_response_body_schema(request_url):
+    data = retrieve_data(request_url)
+    another_path_parameter = True
     response = []
-    res = data.index(u'O1')
-    while anotherPathParameter:
-        response.append([data[res], data[res+1], data[res+2], data[res+3], data[res+4], data[res+5], data[res+6], data[res+7], data[res+8]])
-        if data[res+9] != u'L1': 
+    res = data.index('O1')
+    while another_path_parameter:
+        response.append(
+            [data[res], data[res + 1], data[res + 2], data[res + 3], data[res + 4], data[res + 5], data[res + 6],
+             data[res + 7], data[res + 8]])
+        if data[res + 9] != 'L1':
             res = res + 9
         else:
-            anotherPathParameter = False
+            another_path_parameter = False
     print(response)
-    return(response)
+    return response
 
 
-
-
-
-# This function seperates out the relevent data for the Errors
-def retrieveErrorsList(RequstUrl, lastError):
-    data = retrieveData(RequstUrl)
-    anotherPathParameter = True
+# This function separates out the relevant data for the Errors
+def retrieve_errors_list(request_url, last_error):
+    data = retrieve_data(request_url)
+    another_path_parameter = True
     response = []
-    res = data.index(u'E2')
+    res = data.index('E2')
     res = res - 7
-    while anotherPathParameter:
-        response.append([data[res+2], data[res+3]])
-        if data[res] != lastError: 
+    while another_path_parameter:
+        response.append([data[res + 2], data[res + 3]])
+        if data[res] != last_error:
             res = res + 7
         else:
-            anotherPathParameter = False
+            another_path_parameter = False
     print(response)
-    return(response)
+    return response
 
 
+if __name__ == '__main__':
+    print('---path Parameters---')
+    retrieve_path_parameters(
+        'https://confluence.tools.tax.service.gov.uk/display/MTE/Retrieve+Income+Tax+%28Self+Assessment%29+Crystallisation+Obligations+-+Requirements+Spec'
+    )
 
+    print('---query Parameters---')
+    retrieve_query_parameters(
+        'https://confluence.tools.tax.service.gov.uk/display/MTE/Retrieve+Income+Tax+%28Self+Assessment%29+Crystallisation+Obligations+-+Requirements+Spec'
+    )
+    print('---Request Body---')
+    # request body schema will require close brackets Applying to the Appropriate places, formatting, and required and
+    # additionalProperties fields
+    retrieve_request_body_schema(
+        'https://confluence.tools.tax.service.gov.uk/pages/viewpage.action?spaceKey=MTE&title=Amend+Student+Loans+Example+Requirements+Spec',
+        'From Date'
+    )
 
+    print('---Response Body---')
+    # request body schema will require close brackets Applying to the Appropriate places, formatting, and required and
+    # additionalProperties fields
+    retrieve_response_body_schema(
+        'https://confluence.tools.tax.service.gov.uk/display/MTE/Retrieve+Income+Tax+%28Self+Assessment%29+Crystallisation+Obligations+-+Requirements+Spec'
+    )
 
-
-
-
-
-print('---path Parameters---')
-retrievePathParameters('https://confluence.tools.tax.service.gov.uk/display/MTE/Retrieve+Income+Tax+%28Self+Assessment%29+Crystallisation+Obligations+-+Requirements+Spec')
-print('---query Parameters---')
-retrieveQueryParameters('https://confluence.tools.tax.service.gov.uk/display/MTE/Retrieve+Income+Tax+%28Self+Assessment%29+Crystallisation+Obligations+-+Requirements+Spec')
-print('---Request Body---')
-# request body schema will require close brackets Applying to the Appropriate places, formating, and required and additionalProperties fields
-retrieveRequestBodySchema('https://confluence.tools.tax.service.gov.uk/pages/viewpage.action?spaceKey=MTE&title=Amend+Student+Loans+Example+Requirements+Spec', u'From Date')
-print('---Response Body---')
-# request body schema will require close brackets Applying to the Appropriate places, formating, and required and additionalProperties fields
-retrieveResponseBodySchema('https://confluence.tools.tax.service.gov.uk/display/MTE/Retrieve+Income+Tax+%28Self+Assessment%29+Crystallisation+Obligations+-+Requirements+Spec')
-print('---Errors---')
-retrieveErrorsList('https://confluence.tools.tax.service.gov.uk/display/MTE/Retrieve+Income+Tax+%28Self+Assessment%29+Crystallisation+Obligations+-+Requirements+Spec', u'E6')
+    print('---Errors---')
+    retrieve_errors_list(
+        'https://confluence.tools.tax.service.gov.uk/display/MTE/Retrieve+Income+Tax+%28Self+Assessment%29+Crystallisation+Obligations+-+Requirements+Spec',
+        'E6'
+    )
